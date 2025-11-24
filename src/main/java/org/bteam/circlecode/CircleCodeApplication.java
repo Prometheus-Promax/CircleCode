b233768d25e3e701b236ea7276d2c9288ca110fe
Apollo-pro <luciffercrt@gmail.com>
2025-11-24 23:55:08 +0800

    configurations

---
+++ src/main/java/org/bteam/circlecode/CircleCodeApplication.java
@@ -0,0 +1,13 @@
+package org.bteam.circlecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bteam/circlecode/CircleCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bteam.circlecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
